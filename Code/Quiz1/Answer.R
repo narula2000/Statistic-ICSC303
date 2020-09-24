10b3d33dfc2244fdd5ce96f36a54dc4a87ffc658
Vikrom Narula <djnarula@gmail.com>
2020-09-24 21:39:38 +0700

    Added Quiz 1

--- Code/Quiz1/Answer.R
+++ Code/Quiz1/Answer.R
@@ -1,7 +1,12 @@
-# a) 49557
-10^5 - 22 - 3 * 7^5
-# b) typeof(vec) == "double"
-vec <-  c(3, 5, 8, 5.8, 9)
-# c) mean == 6.16, variance == 5.728
-mean(vec)
-var(vec)
+flu <- c(6, 3, 5, 24, 2, 6, 0, 8)
+mean(flu)
+median(flu)
+
+stu <- c(16, 14, 2, 8, 12, 12, 9, 10, 15, 13)
+median(stu)
+mean(stu)
+var(stu)
+
+mea <- c(5.4, 5.9, 3.5, 4.1, 4.6, 2.5, 4.7, 6.0, 5.4, 4.6, 4.9, 4.6, 4.1, 3.4, 2.2)
+quantile(mea)
+IQR(mea)
